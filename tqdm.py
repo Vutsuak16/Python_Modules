ad2afc99e846d86c47652d95b8939990ea6f31a4
kaustuv deolal <vutsuak96@gmail.com>
2016-02-24 01:50:40 +0530

    tqdm used

---
+++ tqdm.py
@@ -0,0 +1,10 @@
+__name__ = "vutsuak"
+
+from tqdm import tqdm
+from time import sleep
+
+for i in tqdm(range(5)):
+    sleep(.5)
+
+
+# why is this module working only in command line , the IDE shows name error cant import tqdm
