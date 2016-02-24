82f2bb07d5a57910e6821208c4d5361ab34b2b6e
kaustuv deolal <vutsuak96@gmail.com>
2016-02-25 01:17:55 +0530

    fuzzywuzzy used

---
+++ fuzzy_wuzz.py
@@ -0,0 +1,8 @@
+__name__ = "vutsuak"
+
+# remove warning
+# learn more about it
+
+from fuzzywuzzy import fuzz
+
+print fuzz.ratio("kaustuv", "kaustav")
--- tqdm.py
+++ tqdm.py
@@ -7,4 +7,4 @@ for i in tqdm(range(5)):
     sleep(.5)
 
 
-# why is this module working only in command line , the IDE shows name error cant import tqdm
+# why is this module working only in command line , the IDE shows : name error cant import tqdm
